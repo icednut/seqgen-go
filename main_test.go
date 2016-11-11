65cab0c9fb215df2b523b2cad3297cb67eb4d0ca
Wangeun Lee <wglee21g@gmail.com>
2016-11-11 23:39:14 +0900

    fix testcase

--- main_test.go
+++ main_test.go
@@ -37,16 +37,16 @@ func removeDuplicates(elements []int) []int {
 }
 
 func TestRunServer(t *testing.T) {
-    /*go func() {
+    go func() {
         runServer()
-    }()*/
+    }()
 
     arrayCount := 10
     seqNoArray := make([]int, 0)
     for i := 0; i < arrayCount; i++ {
-        go func() {
+        //go func() {
             seqNoArray = append(seqNoArray, callApi("http://localhost:8080"))
-        }()
+        //}()
     }
     uniqueSeqNoArray := removeDuplicates(seqNoArray)
     uniqueSeqNoArrayLen := len(uniqueSeqNoArray)
